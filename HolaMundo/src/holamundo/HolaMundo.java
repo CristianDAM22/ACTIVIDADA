8ede7299e8c69489c6c958ac79cde7bc3445dc40
dam <dam@203P01>
2022-01-20 12:07:02 +0100

    A POR EL BOTE

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -22,24 +22,7 @@ public class HolaMundo {
         System.out.println("Segunda linea");
         System.out.println("Tercero");
         System.out.println("Jessica y john cena");
-        System.out.println("lodsd");
-        System.out.println("sdfjdfkgdf");
-        System.out.println("[Verse 1: Redd4x]\n" +
-"Oye, yo tiene la papele dinero. Que mira que tú ta en... hablando mierda\n" +
-"Mi teléfono que tiene las ñiñas, que tiene o tiene prisosa\n" +
-"Ah, mira tus ojos de linda, que mira la cosa que tiene\n" +
-"La vida, la vida, la vida. El dinero yo tiene, yo tieno\n" +
-"\n" +
-"[Hook: FlySoulja]\n" +
-"Tú quiere mi pinga en tu culo. Ese tú quiere conmigo\n" +
-"\n" +
-"[Verse 2: Redd4x]\n" +
-"Ah, mira que tienе la cosa. Que tiene yo еstá hablando cubano\n" +
-"Está hablando español, sí. Está hablando español, sí\n" +
-"Ya tu abi... tu sabe\n" +
-"\n" +
-"[Hook: FlySoulja]\n" +
-"Te come un trippie en la playa, tú quiere mi pinga en tu culo. Brrr");
+        System.out.println("Prueba de nuevo");
     }
     
 }
